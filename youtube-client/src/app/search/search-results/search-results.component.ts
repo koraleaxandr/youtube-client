6d1c3eb945e3b14af6c79b64da3cb4eaa399fba5
koraleaxandr <koraleaxandr@gmail.com>
2022-04-04 16:53:14 +0300

    fix: edit bottom line coloring settings

--- youtube-client/src/app/search/search-results/search-results.component.ts
+++ youtube-client/src/app/search/search-results/search-results.component.ts
@@ -35,24 +35,29 @@ export class SearchResultsComponent implements OnInit {
   getColorPublicationDate(publishedAt: string): string {
     let color = 'green';
     const millisecondsInWeek: number = 7 * 24 * 60 * 60 * 1000;
-    const today = new Date();
+    const today = new Date('2019-10-15T15:00:14.000Z');
     const publicationDate = new Date(publishedAt);
     const getDateYear = (date: Date): number => Number(date.getUTCFullYear());
-    const getDateMonth = (date: Date): number => Number(date.getUTCMonth() + 1);
+    const getDateMonth = (date: Date): number => (Number(date.getUTCMonth() + 1) + getDateYear(date) * 12);
     const getDateDay = (date: Date): number => Number(date.getUTCDate());
-    if (getDateYear(today) > getDateYear(publicationDate)) {
+
+    if ((getDateMonth(today) - getDateMonth(publicationDate)) > 6) {
       color = 'red';
       return color;
     }
-    if (((getDateMonth(today) - getDateMonth(publicationDate)) >= 6) && (getDateDay(today) > getDateDay(publicationDate))) {
-      color = 'red';
-      return color;
-    }
-    if (getDateMonth(today) > getDateMonth(publicationDate)) {
+    if (((getDateMonth(today) - getDateMonth(publicationDate)) == 6) && (getDateDay(today) < getDateDay(publicationDate))) {
       color = 'yellow';
       return color;
     }
-    if ((Number(today) - Number(publicationDate.getTime())) >= millisecondsInWeek) {
+    if (getDateMonth(today) - getDateMonth(publicationDate) > 1) {
+      color = 'yellow';
+      return color;
+    }
+    if ((getDateMonth(today) - getDateMonth(publicationDate) == 1) && (getDateDay(today) > getDateDay(publicationDate))) {
+      color = 'yellow';
+      return color;
+    }
+    if ((Number(today) - Number(publicationDate.getTime())) <= millisecondsInWeek) {
       color = 'blue';
       return color;
     }
