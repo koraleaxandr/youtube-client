1c5b50aa51119a5e2ba93a83194a3d25e0578877
koraleaxandr <koraleaxandr@gmail.com>
2022-04-18 14:38:33 +0300

    feat: add admin-page

--- youtube-client/src/app/youtube/youtube-routing.module.ts
+++ youtube-client/src/app/youtube/youtube-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
 import { SearchItemComponent } from './pages/search-item/search-item.component';
 import { NotFoundComponent } from '../core/components/not-found/not-found.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 const youtubeRoutes: Routes = [
   { path: '', component: SearchResultsComponent },
   { path: 'search', component: SearchResultsComponent, pathMatch: 'full' },
   { path: 'main', component: SearchResultsComponent, pathMatch: 'full' },
   { path: 'detailed-information/:id', component: SearchItemComponent, pathMatch: 'full' },
+  { path: 'admin-page', component: AdminPageComponent, pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
 
--- youtube-client/src/app/youtube/youtube.module.ts
+++ youtube-client/src/app/youtube/youtube.module.ts
@@ -1,9 +1,8 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormsModule,
-} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
 import { SearchItemComponent } from './pages/search-item/search-item.component';
@@ -11,6 +10,7 @@ import { SearchItemComponent } from './pages/search-item/search-item.component';
 import { YoutubeRoutingModule } from './youtube-routing.module';
 import { ColorOnDateDirective } from './directives/color-on-date.directive';
 import { BoxShadowColorizingDirective } from './directives/box-shadow-colorizing.directive';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +18,14 @@ import { BoxShadowColorizingDirective } from './directives/box-shadow-colorizing
     SearchItemComponent,
     ColorOnDateDirective,
     BoxShadowColorizingDirective,
+    AdminPageComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
     YoutubeRoutingModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
